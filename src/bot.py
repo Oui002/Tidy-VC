f92e24766c131ca63fcbbbe0ddc0b93a6d1983bc
Oui002 <75451291+Oui002@users.noreply.github.com>
2022-03-18 22:44:29 +0100

    initial commit

---
+++ src/bot.py
@@ -0,0 +1,14 @@
+from discord.ext import commands
+
+from src.scripts.client._class import Cli
+
+def main():
+    client = Cli() # commands.Bot(command_prefix = "-")
+
+    @client.event
+    async def on_ready():
+        print("Ready!")
+
+    client.print_config()
+
+    client.run("OTU0NDg2ODAwNzU4NDg5MTMw.YjT1Gg.zpq7t-uzjuU47vjTSWQmV_FmVJk")
---
+++ src/scripts/client/_class.py
@@ -0,0 +1,19 @@
+from discord.ext import commands
+
+from json import load
+
+class Cli(commands.Bot):
+    
+    def __init__(self) -> None:
+        self.config = self.load_config()
+
+        super().__init__(command_prefix=self.config["command_prefix"])
+        
+    def load_config(self) -> object:
+        with open('./src/config/.json', 'r+') as fo:
+            config = load(fo)
+        
+        return config
+
+    def print_config(self) -> None:
+        print(self.config)
