51fd71042c6dc939cc380489baf0a100d7c99c5a
Oui002 <75451291+Oui002@users.noreply.github.com>
2022-03-20 00:22:48 +0100

    minor changes

--- src/scripts/converters/ID2GuildMember.py
+++
@@ -1,4 +0,0 @@
-import discord
-
-def ID2GM(guild: discord.Guild, member_id: int) -> discord.Member:
-    return discord.utils.find(lambda GuildMember: GuildMember.id == member_id, guild.members)
--- src/scripts/utils/new_owner.py
+++ src/scripts/utils/new_owner.py
@@ -1,10 +1,6 @@
 import discord
 
-from src.scripts.converters.ID2GuildMember import ID2GM
-
 def new_owner(channel: discord.VoiceChannel) -> discord.Member:
-    guild = channel.guild
-
     hierarchy = {member.id: member.top_role.position for member in channel.members}
     highest_ranking = 0
 
@@ -15,4 +11,4 @@ def new_owner(channel: discord.VoiceChannel) -> discord.Member:
         except:
             break
 
-    return ID2GM(guild=guild, member_id=highest_ranking)
+    return discord.utils.find(lambda GuildMember: GuildMember.id == member_id, channel.guild.members)
