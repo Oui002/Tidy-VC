2e0e8584fbfe7c8faf97d541e77ff841ae61dbd4
Oui002 <75451291+Oui002@users.noreply.github.com>
2022-03-20 01:21:47 +0100

    minor changes

---
+++ src/scripts/cogs/events/Startup.py
@@ -0,0 +1,18 @@
+from discord.ext import commands
+
+class Startup(commands.Cog):
+
+    def __init__(self, client) -> None:
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_connect(self):
+        print(f"Connected to {self.client.ws}...")
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"Ready as {self.client.user}!")
+
+
+def setup(client: commands.Bot):
+    client.add_cog(Startup(client=client))
